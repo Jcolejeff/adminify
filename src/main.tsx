0f5d07a817ba5045eadb20751a33171c10000d5d
ikwuh jeffery enokela <ikwuhjefferye@gmail.com>
2023-09-07 08:43:42 +0100

    fix : fixed build error

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import 'styles/style.scss';
-import 'react-lazy-load-image-component/src/effects/blur.css';
+
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { UserProvider } from 'context.tsx';
